b2a1f0b6023b30c96b76a9dc300d1e50e8d16bfd
CR1337 <captain_1@gmx.de>
2021-12-10 17:02:01 +0100

    refactored server code, added handle, added determine-rotation endpoint

--- api.py
+++ api.py
@@ -1,214 +1,162 @@
-# based on: https://github.com/serengil/deepface
-
 import warnings
 warnings.filterwarnings("ignore")
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-#------------------------------
-
-from flask import Flask, jsonify, request, make_response
-
-import argparse
-import uuid
-import json
-import time
 import base64
+import json
 from io import BytesIO
-from tqdm import tqdm
-from PIL import Image
-
-#------------------------------
 
 import tensorflow as tf
-tf_version = int(tf.__version__.split(".")[0])
-
-#------------------------------
-
-if tf_version == 2:
-	import logging
-	tf.get_logger().setLevel(logging.ERROR)
-
-#------------------------------
-
 from deepface import DeepFace
+from flask import Flask, jsonify, request
+from PIL import Image
 
-#------------------------------
+tf_version = int(tf.__version__.split(".")[0])
+if tf_version == 2:
+    import logging
+    tf.get_logger().setLevel(logging.ERROR)
+if tf_version == 1:
+    graph = tf.get_default_graph()
 
 app = Flask(__name__)
 
-#------------------------------
+OK_200 = 200
+BAD_REQUEST_400 = 400
 
-if tf_version == 1:
-	graph = tf.get_default_graph()
 
-#------------------------------
+def with_deepface(func):
+    def wrapper(*args, **kwargs):
+        global graph
+        if tf_version == 1:
+            with graph.as_default():
+                return func(*args, **kwargs)
+        else:
+            return func(*args, **kwargs)
+    return wrapper
+
+
+@with_deepface
+def detect_face(image):
+    return DeepFace.detectFace(image)
+
+
+@with_deepface
+def analyze_face(image, actions):
+    return DeepFace.analyze(image, actions)
+
+
+@with_deepface
+def verify_face(image0, image1):
+    return DeepFace.verify(image0, image1)
+
 
 def rotate_image(image, angle):
-	im = Image.open(BytesIO(base64.b64decode(image.split(",")[1])))
-	im = im.rotate(angle)
-	in_mem_file = BytesIO()
-	im.save(in_mem_file, format="JPEG")
-	# reset file pointer to start
-	in_mem_file.seek(0)
-	img_bytes = in_mem_file.read()
-	base64_encoded_result_bytes = base64.b64encode(img_bytes)
-	base64_encoded_result_str = base64_encoded_result_bytes.decode('ascii')
-	return ",".join([image.split(",")[0], base64_encoded_result_str])
+    if angle == 0:
+        return image
+    im = Image.open(BytesIO(base64.b64decode(image.split(",")[1])))
+    im = im.rotate(angle)
+    in_mem_file = BytesIO()
+    im.save(in_mem_file, format="JPEG")
+    in_mem_file.seek(0)
+    img_bytes = in_mem_file.read()
+    base64_encoded_result_bytes = base64.b64encode(img_bytes)
+    base64_encoded_result_str = base64_encoded_result_bytes.decode('ascii')
+    return ",".join([image.split(",")[0], base64_encoded_result_str])
 
 
-#Service API Interface
+def determine_rotation_angle(image):
+    for angle in [0, 90, 180, 270]:
+        rotated_image = rotate_image(image, angle) if angle > 0 else image
+        try:
+            detect_face(rotated_image)
+        except ValueError:
+            pass
+        else:
+            return {'angle': angle}
+    raise ValueError()
 
-@app.route('/')
+
+def api_route(func):
+    def wrapper():
+        request_data = json.loads(request.data.decode(encoding='utf-8'))
+
+        if 'handle' not in request_data:
+            return (
+                jsonify({'success': False, 'error': "no handle"}),
+                BAD_REQUEST_400
+            )
+        else:
+            handle = request_data['handle']
+
+        result, code = func(request_data)
+
+        result['handle'] = handle
+        result['success'] = (code == OK_200)
+
+        return jsonify(result), code
+    return wrapper
+
+
+@app.route('/', methods=['GET'], endpoint='index')
 def index():
-	return '<h1>Hello, world!</h1>'
-
-@app.route('/analyze', methods=['POST'])
-def analyze():
-
-	global graph
-
-	tic = time.time()
-	# req = request.get_json()
-	req = json.loads(request.data.decode(encoding='utf-8'))
-	trx_id = uuid.uuid4()
+    return '<h1>Hello, world!</h1>', OK_200
 
 
+@app.route('/analyze', methods=['POST'], endpoint='analyze')
+@api_route
+def analyze(request_data):
+    angle = request_data.get('angle', 0)
 
-	#---------------------------
+    if 'img' not in request_data:
+        return {'error': "no img"}, BAD_REQUEST_400
+    image = rotate_image(request_data['img'], angle)
 
-	if tf_version == 1:
-		with graph.as_default():
-			resp_obj, return_code = analyzeWrapper(req, trx_id)
-	elif tf_version == 2:
-		resp_obj, return_code = analyzeWrapper(req, trx_id)
+    actions = request_data.get(
+        'actions', ['emotion', 'age', 'gender', 'race']
+    )
 
-	#---------------------------
-
-	toc = time.time()
-
-	resp_obj["trx_id"] = trx_id
-	resp_obj["seconds"] = toc-tic
-
-	return resp_obj, return_code
-
-def analyzeWrapper(req, trx_id = 0):
-	if "img" in list(req.keys()):
-		raw_content = req["img"] #list
-
-		# TODO: This needs a very good comment
-		angle = 0 if 'angle' not in req else req['angle']
-		raw_content = rotate_image(raw_content, angle)
+    try:
+        result = analyze_face(image, actions)
+    except ValueError:
+        return {'error': 'no face'}, BAD_REQUEST_400
+    else:
+        return result, OK_200
 
 
-	else:
-		return jsonify({'success': False, 'error': 'you must pass an img object in your request'}), 205
+@app.route('/verify', methods=['POST'], endpoint='verify')
+@api_route
+def verify(request_data):
+    angle = request_data.get('angle', 0)
 
-	print("Analyzing instance")
+    images = [None, None]
+    for i, key in enumerate(['img0', 'img1']):
+        if key not in request_data:
+            return {'error': f"no img{i}"}, BAD_REQUEST_400
+        images[i] = rotate_image(request_data[key], angle)
 
-	#---------------------------
-
-	detector_backend = 'opencv'
-
-	actions= ['emotion', 'age', 'gender', 'race']
-
-	if "actions" in req:
-		actions = req["actions"]
-
-
-	#---------------------------
-	resp_obj = {}
-	try:
-		resp_obj = DeepFace.analyze(raw_content, actions=actions)
-		return resp_obj, 200
-	except Exception as err:
-		print("Exception: ", str(err))
-		resp_obj = {'error': str(err)}
-		return resp_obj, 205
+    try:
+        result = verify_face(images[0], images[1])
+    except ValueError:
+        return {'error': 'no face'}, BAD_REQUEST_400
+    else:
+        return result, OK_200
 
 
-@app.route('/verify', methods=['POST'])
-def verify():
+@app.route('/determine-angle', methods=['POST'], endpoint='determine_angle')
+@api_route
+def determine_angle(request_data):
+    if 'img' not in request_data:
+        return {'error': "no img"}, BAD_REQUEST_400
+    image = request_data['img']
 
-	global graph
-
-	tic = time.time()
-	req = request.get_json()
-	trx_id = uuid.uuid4()
-
-	resp_obj = jsonify({'success': False})
-
-	if tf_version == 1:
-		with graph.as_default():
-			resp_obj, return_code = verifyWrapper(req, trx_id)
-	elif tf_version == 2:
-		resp_obj, return_code = verifyWrapper(req, trx_id)
-
-	#--------------------------
-
-	toc =  time.time()
-
-	resp_obj["trx_id"] = trx_id
-	resp_obj["seconds"] = toc-tic
-
-	return resp_obj, return_code
-
-def verifyWrapper(req, trx_id = 0):
-
-	resp_obj = jsonify({'success': False})
-
-	model_name = "VGG-Face"
-	distance_metric = "cosine"
-	detector_backend = "opencv"
-
-
-	#----------------------
-
-	if "img" in list(req.keys()):
-		raw_content = req["img"] #list
-
-		instance = []
-		img1 = raw_content["img1"]; img2 = raw_content["img2"]
-
-		angle = 0 if 'angle' not in req else req['angle']
-		img1 = rotate_image(img1, angle)
-		img2 = rotate_image(img2, angle)
-
-
-		validate_img1 = False
-		if len(img1) > 11 and img1[0:11] == "data:image/":
-			validate_img1 = True
-
-		validate_img2 = False
-		if len(img2) > 11 and img2[0:11] == "data:image/":
-			validate_img2 = True
-
-		if validate_img1 != True or validate_img2 != True:
-			return jsonify({'success': False, 'error': 'you must pass both img1 and img2 as base64 encoded string'}), 205
-
-		instance.append(img1); instance.append(img2)
-
-	else:
-		return jsonify({'success': False, 'error': 'you must pass two img objects in your request'}), 205
-
-	#--------------------------
-
-	print("Verifying instance")
-	#--------------------------
-
-	try:
-		resp_obj = DeepFace.verify(instance
-			, model_name = model_name
-			, distance_metric = distance_metric
-			, detector_backend = detector_backend
-		)
-
-	except Exception as err:
-		resp_obj = jsonify({'success': False, 'error': str(err)}), 205
-
-	return resp_obj, 200
+    try:
+        result = determine_rotation_angle(image)
+    except ValueError:
+        return {'error': 'no face'}, BAD_REQUEST_400
+    else:
+        return result, OK_200
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
